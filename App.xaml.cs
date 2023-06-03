ee51191ca1288fe473f04c74e57719202fbc7a69
Chealy Te <chealyte@hotmail.com>
2023-06-03 18:26:04 -0500

    06 03 2023 6:30 PM just visual change to view file to make it look nice.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,43 @@
 ﻿namespace MyCoffeeMVVMSQLite;
 
+/*
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+*/
+
 public partial class App : Application
 {
-	public App()
+    // windows size 1920 x 1080 = 1080P
+	const int WindowWidth = 1920;
+    const int WindowHeight = 1080;
+
+    public App()
 	{
 		InitializeComponent();
+/*
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+#if WINDOWS
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            
+            nativeWindow.Activate();
+            
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
 
-		MainPage = new AppShell();
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+            
+            
+            // HMMM
+       
+#endif
+        });
+*/
+        MainPage = new AppShell();
 	}
 }
